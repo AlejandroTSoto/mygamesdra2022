74a9285830c15c34b0859b9e1e660fe59ce53c35
ALEJANDRO TOMAS SOTO MECA <asm978@inlumine.ual.es>
2022-05-27 18:36:21 +0200

    Cambio en el contenido

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,27 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContentComponent } from './content/content.component';
-import { MenuComponent } from './menu/menu.component';
-import { GarageComponent } from './garage/garage.component';
-import { MotorComponent } from './motor/motor.component';
-import { PinturaComponent } from './pintura/pintura.component';
-import { LlantaComponent } from './llanta/llanta.component';
-import { TapiceriaComponent } from './tapiceria/tapiceria.component';
-import { AsientoComponent } from './asiento/asiento.component';
-
-import { ConfiguratorComponent } from './configurator/configurator.component';
 
 const routes: Routes = [
-  {path:'',component:ContentComponent},
-  {path:'home',component:ContentComponent},
-  {path: 'menu', component:MenuComponent},
-  {path:'configurator/:id',component:ConfiguratorComponent},
-  {path:'garage', component:GarageComponent},
-  {path:'motor', component: MotorComponent},
-  {path:'pintura', component: PinturaComponent},
-  {path:'llanta', component:LlantaComponent},
-  {path:'tapiceria', component:TapiceriaComponent},
-  {path:'asiento', component: AsientoComponent}
+
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -14,35 +12,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
-import { MotorComponent } from './motor/motor.component';
-import { ConfiguracionTarjetaComponent } from './configuracion-tarjeta/configuracion-tarjeta.component';
-import { TapiceriaComponent } from './tapiceria/tapiceria.component';
-import { CarComponent } from './car/coche.component';
-import { GarageComponent } from './garage/garage.component';
-import { ContentComponent } from './content/content.component';
-import { MenuComponent } from './menu/menu.component';
-import { ConfiguratorComponent } from './configurator/configurator.component';
-import { PinturaComponent } from './pintura/pintura.component';
-import { AsientoComponent } from './asiento/asiento.component';
-import { LlantaComponent } from './llanta/llanta.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    NavComponent,
-    MotorComponent,
-    ConfiguracionTarjetaComponent,
-    TapiceriaComponent,
-    CarComponent,
-    GarageComponent,
-    ContentComponent,
-    MenuComponent,
-    ConfiguratorComponent,
-    PinturaComponent,
-    AsientoComponent,
-    LlantaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
